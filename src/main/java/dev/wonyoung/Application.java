21b3fcbcab61e47a9e36390da4bd20cdf6faf669
hexter31376 <zwonyoung33@gmail.com>
2026-03-09 22:16:26 +0900

    chore: 기본 ConsoleView.java 제거

--- src/main/java/dev/wonyoung/Application.java
+++ src/main/java/dev/wonyoung/Application.java
@@ -6,9 +6,5 @@ public class Application {
     public static void main(String[] args) throws Exception {
         Container container = new Container();
         container.scan("dev.wonyoung");
-
-        // 예시: ConsoleView 인스턴스 가져오기
-        var consoleView = container.get(dev.wonyoung.adapter.in.console.ConsoleView.class);
-        consoleView.display();
     }
 }
